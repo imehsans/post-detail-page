5cb7d99d7a21c6bf7532abcbfb64ea7d04aa6224
Aleem A <mehsanjadoon786@gmail.com>
2023-07-14 07:50:19 +0500

    updates

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,14 +5,14 @@ import PostDetails from './sub_components/PostDetails'
 import PostHeader from './sub_components/PostHeader'
 const Post = () => {
    return (
-      <>
+      <div className='post-main'>
          <PostHeader />
          <PostDetails />
          <div className='post-img'>
             <Icon src="/images/postimage.jpg" />
          </div>
          <LikesAndComments />
-      </>
+      </div>
    )
 }
 
